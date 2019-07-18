8bb93a0ed13fb6f2507b9ef33765728822159759
megaturbo <megaturbo@truelevel.io>
2019-07-18 17:17:45 +0200

    Use bn.js to calculatePrice and add doc

--- src/calculatePrice.ts
+++ src/calculatePrice.ts
@@ -1,27 +1,40 @@
-export const tokenPrice = (input_reserve: any, output_reserve: any, input_amount: any) => {
-  try {
-    const input_amount_with_fee = input_amount * 10**18 * 997;
+import BN from 'bn.js';
+import { fromWei, toWei } from 'web3x/utils';
 
-    const numerator = input_amount_with_fee * output_reserve;
-    const denominator = (input_reserve *1000) + input_amount_with_fee;
-
-
-
-    return (numerator / denominator) / 10**18;
-  } catch (err) {
-    throw err
+/**
+ * Pricing function for Token price.
+ *
+ * @param inputReserve  Input amount of ETH in exchange reserves.
+ * @param outputReserve Output amount of Token in exchange reserves.
+ * @param inputAmount   Amount of ETH being sold
+ */
+export const tokenPrice = (inputReserve: BN, outputReserve: BN, inputAmount: BN)=> {
+  if (!inputReserve.gtn(0) || !outputReserve.gtn(0)) {
+    throw Error('Reserves should be greater than zero');
   }
+
+  const inputAmount_with_fee = toWei(inputAmount, 'ether').muln(997);
+
+  const numerator = inputAmount_with_fee.mul(outputReserve);
+  const denominator = inputReserve.muln(1000).add(inputAmount_with_fee);
+  return fromWei(numerator.div(denominator), 'ether');
 }
 
-export const ethPrice = (input_reserve: any, output_reserve: any, output_amount: any) => {
-  try{
-    const output_amount_int = output_amount * 10**18;
-
-    const numerator = input_reserve * output_amount_int * 1000;
-    const denominator = (output_reserve - output_amount_int) * 997;
-
-    return (numerator / denominator + 1) / 10**18;
-  } catch (err) {
-    throw err
+/**
+ * Pricing function for ETH price.
+ *
+ * @param inputReserve  Input amount of ETH in exchange reserves.
+ * @param outputReserve Output amount of Token in exchange reserves.
+ * @param outputAmount  Amount of ETH being bought
+ */
+export const ethPrice = (inputReserve: BN, outputReserve: BN, outputAmount: BN) => {
+  if (!inputReserve.gtn(0) || !outputReserve.gtn(0)) {
+    throw Error('Reserves should be greater than zero');
   }
+
+  const outputAmountInt = toWei(outputAmount, 'ether')
+  const numerator = inputReserve.mul(outputAmountInt).muln(1000);
+  const denominator = outputReserve.sub(outputAmountInt).muln(997);
+
+  return fromWei(numerator.div(denominator).addn(1), 'ether');
 }

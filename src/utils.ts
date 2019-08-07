f68ca41f440e55a9af39967f847fe167ec1740b8
megaturbo <megaturbo@truelevel.io>
2019-08-07 14:36:58 +0200

    Add and human readable address format to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,13 @@
 import BN from "bn.js";
+import { Address } from "web3x/address";
 import { fromWei } from "web3x/utils";
 
 export const weiToHuman = (weiAmount: BN, fixed = 3) => {
   return Number(fromWei(weiAmount.toString(), "ether")).toFixed(fixed);
+};
+
+export const addressToHuman = (address: Address) => {
+  const a = address.toString();
+  const len = a.length;
+  return a.slice(0, 5) + "..." + a.slice(len - 5);
 };

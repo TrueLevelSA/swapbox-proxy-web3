11f8a450b6e25494dabadcca856d1c67dcce654f
megaturbo <megaturbo@truelevel.io>
2019-08-12 15:30:53 +0200

    Fix buy ETH order

--- src/processing/orders.ts
+++ src/processing/orders.ts
@@ -2,34 +2,39 @@ import BN from "bn.js";
 import { Address } from "web3x/address";
 import { toWei } from "web3x/utils";
 
-import { Atola } from "../contracts/Atola";
-import { addressToHuman } from "../utils";
+import { Atola, CryptoPurchaseEvent } from "../contracts/Atola";
+import { addressToHuman, weiToHuman } from "../utils";
 
 /**
  * Send a buy order to the Atola contract.
  *
- * @param atola        Atola contract instance.
- * @param from         Address from which the tx are sent (machine address).
- * @param amount       The amount of Fiat money received in the machine.
- * @param userAddress  Address on which the exchange will deposit the Eth.
+ * @param atola           Atola contract instance.
+ * @param machineAddress  Address from which the tx are sent (machine address).
+ * @param amount          The amount of Fiat money received in the machine.
+ * @param userAddress     Address on which the exchange will deposit the Eth.
  */
-export async function processBuyEthOrder(atola: Atola, from: Address, amount: BN, userAddress: Address) {
+export async function processBuyEthOrder(
+  atola: Atola,
+  machineAddress: Address,
+  amount: BN,
+  minEth: BN,
+  userAddress: Address,
+) {
   console.log();
-  console.log(`order: ${amount} ETH from: ${addressToHuman(from)} to: ${addressToHuman(userAddress)}`);
+  console.log(`order: ${weiToHuman(amount)} ETH for: ${addressToHuman(userAddress)}`);
+
   try {
-    const estimate = await atola.methods.fiatToEth(toWei(amount, "ether"), 10000, userAddress).estimateGas();
-
     const fiatToEth = await atola.methods.fiatToEth(
-      toWei(amount, "ether"), 10000, userAddress,
-    ).send({ from }).getReceipt();
+      amount, minEth, userAddress,
+    ).send({ from: machineAddress }).getReceipt();
     console.log(fiatToEth.events);
-
+    if (fiatToEth.events) {
+      return fiatToEth.events.CryptoPurchase[0].returnValues;
+    }
   } catch (e) {
     console.error("Error while processBuyEthOrder");
     console.error(e);
   }
-  // TODO: wait for Atola event `CryptoPurchase` and return bought ETH amount.
-  return new BN(1234);
 }
 
 /**

de2144a414dc118da9ea51aded2423d85fcfe822
T <info@swapbox.cash>
2024-02-19 10:26:05 -0300

    change test deploy to USDT and fund swapbox contract

--- scripts/deploy_swapbox.ts
+++ scripts/deploy_swapbox.ts
@@ -10,7 +10,8 @@ async function main() {
     const [deployer] = await ethers.getSigners();
 
     // That's mainnet addresses.
-    const baseToken = "0xb4272071ecadd69d933adcd19ca99fe80664fc08";
+    // const baseToken = "0xb4272071ecadd69d933adcd19ca99fe80664fc08"; // XCHF
+    const baseToken = "0xdac17f958d2ee523a2206206994597c13d831ec7"; // USDT
     const wethToken = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
     const factory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
     const router = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
@@ -30,6 +31,16 @@ async function main() {
 
     await swapbox.addToken(wethToken, override);
     console.log(`Added token: ${wethToken}`);
+
+    // get some basetoken for testing
+    const usdt_signer = await ethers.getImpersonatedSigner("0x5754284f345afc66a98fbB0a0Afe71e0F007B949");
+    const amount = ethers.utils.parseUnits("1000", 6); // USDT typically has 6 decimals
+    const usdtAddress = "0xdac17f958d2ee523a2206206994597c13d831ec7";
+    const usdtABI = ["function transfer(address to, uint amount) returns (bool)"];
+    const usdtContract = new ethers.Contract(usdtAddress, usdtABI, usdt_signer);
+
+    await usdtContract.transfer(swapbox.address, amount);
+    console.log(`Transferred ${amount} USDT to ${swapbox.address}`);
 
     const pricefeed = await pricefeedFactory.deploy(
         swapbox.address,
